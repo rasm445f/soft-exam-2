275ba523b413813cfbeb87f704d441a70a3793fa
Oscar Tuff <90379013+oTuff@users.noreply.github.com>
2024-11-17 20:37:27 +0100

    Add test for viewing order and update model

--- services/shoppingCartService/db/models.go
+++ services/shoppingCartService/db/models.go
@@ -1,7 +1,18 @@
 package db
 
+// shopping cart new restaurantid
+type ShoppingCart struct {
+	Id           string             `json:"id"`
+	UserId       int                `json:"user_id"`
+	RestaurantId int                `json:"restaurant_id"`
+	TotalAmount  int                `json:"total_amount"`
+	VatAmount    int                `json:"vat_amount"`
+	Comment      string             `json:"comment"`
+	Items        []ShoppingCartItem `json:"items"`
+}
+
 type ShoppingCartItem struct {
-	ID       string  `json:"id"`
+	Id       string  `json:"id"`
 	Name     string  `json:"name"`
 	Price    float64 `json:"price"`
 	Quantity int     `json:"quantity"`
