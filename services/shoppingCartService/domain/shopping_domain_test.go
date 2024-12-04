328fdcc0d74ae86a7dd6eb53f5056e0880bd7662
Oscar Tuff <90379013+oTuff@users.noreply.github.com>
2024-12-04 12:02:15 +0100

    Add more tests for domain

--- services/shoppingCartService/domain/shopping_domain_test.go
+++ services/shoppingCartService/domain/shopping_domain_test.go
@@ -28,7 +28,7 @@ func TestAddItemDomain(t *testing.T) {
 			{
 				Id:       1,
 				Name:     "Sample Item",
-				Price:    30,
+				Price:    30.0,
 				Quantity: 2,
 			},
 		},
@@ -65,31 +65,54 @@ func TestAddItemDomain(t *testing.T) {
 			t.Errorf("unmet expectations: %s", err)
 		}
 	})
-	// t.Run("successfully Add item to existing cart", func(t *testing.T) {
-	// 	// first create a cart like above
-	// 	cartKey := "cart:123"
-	// 	// first we will get the RedisNil error because the cart does not exist
-	// 	mock.ExpectGet(cartKey).RedisNil()
-	// 	mock.ExpectSet(cartKey, cartData, 0).SetVal("OK")
-	// 	err := domain.AddItemDomain(context.Background(), itemParams)
-	// 	if err != nil {
-	// 		t.Errorf("unexpected error: %v", err)
-	// 	}
-	//
-	// 	cart.TotalAmount += itemParams.Price
-	// 	cart.VatAmount += itemParams.Price * 0.2
-	//
-	// 	cartData, err := json.Marshal(cart)
-	// 	if err != nil {
-	// 		t.Fatalf("unexpected error marshalling cart: %v", err)
-	// 	}
-	//
-	// 	// insert another item in the same cart
-	// 	err = domain.AddItemDomain(context.Background(), itemParams)
-	// 	if err != nil {
-	// 		t.Errorf("unexpected error: %v", err)
-	// 	}
-	//
-	// })
-	t.Run("check quantity", func(t *testing.T) {})
+	t.Run("successfully Add item to existing cart", func(t *testing.T) {
+		// first create a cart like above
+		cartKey := "cart:123"
+		// first we will get the RedisNil error because the cart does not exist
+		mock.ExpectGet(cartKey).RedisNil()
+		mock.ExpectSet(cartKey, cartData, 0).SetVal("OK")
+		err := domain.AddItemDomain(context.Background(), itemParams)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		// the expected new cart data
+		newItem := db.ShoppingCartItem{
+			Id:       len(cart.Items) + 1,
+			Name:     itemParams.Name,
+			Price:    itemParams.Price,
+			Quantity: itemParams.Quantity,
+		}
+		cart.Items = append(cart.Items, newItem)
+		cart.TotalAmount += newItem.Price * float64(newItem.Quantity)
+		cart.VatAmount += newItem.Price * float64(newItem.Quantity) * 0.2
+
+		newCartData, err := json.Marshal(cart)
+		if err != nil {
+			t.Fatalf("unexpected error marshalling cart: %v", err)
+		}
+
+		// calling ExpectGet should return the current cartData
+		mock.ExpectGet(cartKey).SetVal(string(cartData))
+		// When calling  adding another domain newCartData is expected
+		mock.ExpectSet(cartKey, newCartData, 0).SetVal("OK")
+		// insert another item in the same cart
+		err = domain.AddItemDomain(context.Background(), itemParams)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+	})
+	t.Run("check quantity", func(t *testing.T) {
+		itemParams.Quantity = 0
+
+		// should return error when quantity is set to 0
+		err := domain.AddItemDomain(context.Background(), itemParams)
+		if err == nil {
+			t.Errorf("expected error: %v", err)
+		}
+
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("unmet expectations: %s", err)
+		}
+	})
 }
